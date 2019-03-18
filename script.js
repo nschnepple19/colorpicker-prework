599f4daf2cd66e10306fb661cd805f5bec5138e5
NickSchnepple <nschnepple@cnm.edu>
2019-03-18 15:11:13 -0600

    added rgb to each Event Listener

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ function initColorPicker() {
 function setColorPickerEventListeners(colorBox, rgb) {
 	rgb.red.addEventListener('change', () => {
 		console.log("Red value: ", rgb.red.value);
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.green.addEventListener('change', () => {
 		console.log("Green value: ", rgb.green.value);
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.blue.addEventListener('change', () => {
 		console.log("Blue value: ", rgb.blue.value);
+		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 }
 
