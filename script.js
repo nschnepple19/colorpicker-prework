f710851faf796fee4e318e4b7fc6476decd64fe6
NickSchnepple <nschnepple@cnm.edu>
2019-03-18 15:39:21 -0600

    created for loop and refactored

--- script.js
+++ script.js
@@ -9,23 +9,22 @@ function initColorPicker() {
 		green: document.getElementById("green"),
 		blue: document.getElementById("blue")
 	};
-	setColorPickerEventListeners(colorBox, rgb);
+	let colorPickers = document.getElementsByClassName("picker")
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb) {
-	rgb.red.addEventListener('change', () => {
-		console.log("Red value: ", rgb.red.value);
-		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-	});
-	rgb.green.addEventListener('change', () => {
-		console.log("Green value: ", rgb.green.value);
-		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-	});
-	rgb.blue.addEventListener('change', () => {
-		console.log("Blue value: ", rgb.blue.value);
-		setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-	});
-}
+function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+	let pickerLen =pickerElements.length
+	for (let i = 0; i < pickerLen; i++) {
+		pickerElements[i].addEventListener('change', () => {
+			console.log("Red value: ", rgb.red.value);
+			let red = rgb.red.value;
+			let green = rgb.green.value
+			let blue = rgb.blue.value
+			setBoxBGColor(colorBox, red, green, blue);
+			})
+		}
+	}
 
 function setBoxBGColor(colorBox, red, green, blue) {
 	rgbVal = [red, green, blue].join(',');
